a3458a218324e90e37ee57beae714dda6d74acc0
1-gagandeep <112457883+1-gagandeep@users.noreply.github.com>
2023-08-02 13:18:48 +0530

    Updated

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {WebView} from 'react-native-webview';
+
+const HomeScreen = ({navigation}) => (
+  <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+    <Text>This is the home screen</Text>
+    <TouchableOpacity onPress={ () => navigation.navigate("Chat") }> 
+      <Text style={{color:'lightgreen', backgroundColor:'green'}} >Open Live Chat</Text>
+       </TouchableOpacity>
+  </View>
+);
+
+const ChatScreen = () => (
+  <View style={{flex:1}}>
+    <WebView source={{uri:'https://tawk.to/chat/6436ef8731ebfa0fe7f7f086/1gtr9fc12'}} />
+    
+  </View>
+);
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
